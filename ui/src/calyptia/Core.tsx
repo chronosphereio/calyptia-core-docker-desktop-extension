c4ca1a52a660d47f8ca3acc6f049dca7d05a09ec
Patrick Stephens <pat@calyptia.com>
2022-10-07 16:14:17 +0100

    Ensure we extract local core instance info
    Signed-off-by: Patrick Stephens <pat@calyptia.com>

--- ui/src/calyptia/Core.tsx
+++ ui/src/calyptia/Core.tsx
@@ -9,19 +9,20 @@ import ErrorIcon from '@mui/icons-material/Error';
 // If you're running this React app in a browser, it won't work properly.
 const client = createDockerDesktopClient();
 
-export const DockerDesktop = "docker-desktop";
-export const CurrentExtensionContext = "currentExtensionContext"
-export const IsK8sEnabled = "isK8sEnabled"
+const DockerDesktop = "docker-desktop";
+const CurrentExtensionContext = "currentExtensionContext"
+const IsK8sEnabled = "isK8sEnabled"
 
 const isK8sEnabled = () => {
   return localStorage.getItem(IsK8sEnabled) === "true";
 }
 
-const refreshData = async (setCurrentHostContext: React.Dispatch<React.SetStateAction<any>>) => {
+const refreshData = async (setCurrentHostContext: React.Dispatch<React.SetStateAction<any>>, setCoreInstanceInfo: React.Dispatch<React.SetStateAction<any>>) => {
     try {
         if (isK8sEnabled()) {
-            const result = await Promise.all([getCurrentHostContext(client)]);
+            const result = await Promise.all([getCurrentHostContext(client),getCoreInfo(client)]);
             setCurrentHostContext(result[0]);
+            setCoreInstanceInfo(result[1])
         }
     } catch (err: any) {
         if ("stdout" in err && err.stdout.includes("fatal")) {
@@ -31,14 +32,8 @@ const refreshData = async (setCurrentHostContext: React.Dispatch<React.SetStateA
     }
 }
 
-// set context on extension container
-export const setExtensionContext = async (ddClient: v1.DockerDesktopClient, context: string) => {
-  // update current extension context using localstorage
-  localStorage.setItem(CurrentExtensionContext, context);
-}
-
 // Change context on extension container
-export const getExtensionContext = () => {
+const getExtensionContext = () => {
   // retrieve extension current context
   return localStorage.getItem(CurrentExtensionContext) || DockerDesktop;
 }
@@ -50,7 +45,7 @@ const hostCli = async (ddClient: v1.DockerDesktopClient, command: string, args: 
 
 
 // Retrieves host's current k8s context
-export const getCurrentHostContext = async (ddClient: v1.DockerDesktopClient) => {
+const getCurrentHostContext = async (ddClient: v1.DockerDesktopClient) => {
   // kubectl config view -o jsonpath='{.current-context}'
   let output = await hostCli(ddClient, "kubectl", ["config", "view", "-o", "jsonpath='{.current-context}'"]);
   if (output?.stderr) {
@@ -61,7 +56,7 @@ export const getCurrentHostContext = async (ddClient: v1.DockerDesktopClient) =>
 }
 
 // Retrieves `kubectl cluster-info` context-wise
-export const checkK8sConnection = async (ddClient: v1.DockerDesktopClient) => {
+const checkK8sConnection = async (ddClient: v1.DockerDesktopClient) => {
   // kubectl cluster-info --context context-name
   try {
       let output = await hostCli(ddClient, "kubectl", ["cluster-info", "--request-timeout", "2s", "--context", getExtensionContext()]);
@@ -82,8 +77,17 @@ export const checkK8sConnection = async (ddClient: v1.DockerDesktopClient) => {
   }
 }
 
+const getCoreInfo = async (ddClient: v1.DockerDesktopClient) => {
+// Get the UUID from the first deployment with the relevant lablel
+    let output = await hostCli(ddClient, "kubectl", ["get", "deployments", "-l", "calyptia_aggregator_id", "--output=jsonpath={.items[0].metadata.labels.calyptia_aggregator_id}"] );
+    if (output?.stderr) {
+        console.log("[getCoreInfo] : ", output.stderr);
+        return {};
+    }
+    return output?.stdout;
+}
+
 export const Core = () => {
-  const [isLoading, setIsLoading] = React.useState(false);
   const [currentHostContext, setCurrentHostContext] = React.useState("");
   const [projectToken, setProjectToken] = React.useState<string | undefined>();
   const [coreInstanceInfo, setCoreInstanceInfo] = React.useState<string | undefined>();
@@ -92,18 +96,18 @@ export const Core = () => {
     (async () => {
         // @ts-ignore
         checkK8sConnection(client);
-        await refreshData(setCurrentHostContext)
+        await refreshData(setCurrentHostContext, setCoreInstanceInfo)
     })();
 
     const dataInterval = setInterval(() => {
-        return refreshData(setCurrentHostContext)
+        return refreshData(setCurrentHostContext, setCoreInstanceInfo)
     }, 5000);
 
     return () => {
         clearInterval(dataInterval);
     }
 }, []);
-    const runCore = async (ddClient: v1.DockerDesktopClient, args: string[]) => {
+    const createCoreInstance = async (ddClient: v1.DockerDesktopClient, args: string[]) => {
         setCoreInstanceInfo(null)
         let output = await hostCli(ddClient, "calyptia", args);
 
@@ -111,19 +115,16 @@ export const Core = () => {
             console.log(output.stderr);
             return false;
         }
-        if (output?.stdout) {
-            setCoreInstanceInfo(output.stdout)
-        }
         return true;
     }
 
-  const uiRunCore = async () => {
+  const uiCreateCoreInstance = async () => {
     if ( projectToken === "" ) {
         client.desktopUI.toast.error("no project token");   
     } else {
         try {
             let args = ["create", "core_instance", "kubernetes", "--token", projectToken]
-            const isCreated = await runCore(client, args);
+            const isCreated = await createCoreInstance(client, args);
             if (isCreated) {
                 client.desktopUI.toast.success("core instance creation successful");
             } else {
@@ -135,20 +136,7 @@ export const Core = () => {
     }
 };
 
-  let component
-  if (isLoading) {
-    component = <Box sx={{
-        marginBottom: "15px",
-        textAlign: "center"
-    }}>
-        <CircularProgress
-            size={50}
-            sx={{
-                color: blueGrey[500],
-            }}
-        />
-    </Box>
-} else {
+    let component
     if (isK8sEnabled()) {
         // client.host.openExternal("https://cloud.calyptia.com");
         component =<Stack direction="row" spacing={2}>
@@ -164,43 +152,42 @@ export const Core = () => {
         size="medium"
         fullWidth
         required/>
-        <Button onClick={uiRunCore} color="primary" variant="outlined">
+        <Button onClick={uiCreateCoreInstance} color="primary" variant="outlined">
             Create Core Instance
         </Button>
         {coreInstanceInfo ? (
         <div>
-          <Typography>Core Instance Info:</Typography>
-          <Typography>{coreInstanceInfo}</Typography>
+            <Typography>Core Instance Info:</Typography>
+            <Typography>{coreInstanceInfo}</Typography>
         </div>
         ) : (
             ''
-          )}
+            )}
     </Stack>
         } else {
-      component = <Box>
-      <Alert iconMapping={{
-          error: <ErrorIcon fontSize="inherit"/>,
-      }} severity="error" color="error">
-          Seems like Kubernetes is not reachable from your Docker Desktop.
-          Please take a look at the <a href="https://docs.docker.com/desktop/kubernetes/">docker
-          documentation</a> on how to enable the Kubernetes server in docker-desktop and then select
-          docker-desktop context.
-          For other type of k8s clusters, check if kube-apiserver of your k8s cluster is reachable from your
-          host machine.
-      </Alert>
-  </Box>
-      }
-  return (
+        component = <Box>
+        <Alert iconMapping={{
+            error: <ErrorIcon fontSize="inherit"/>,
+        }} severity="error" color="error">
+            Seems like Kubernetes is not reachable from your Docker Desktop.
+            Please take a look at the <a href="https://docs.docker.com/desktop/kubernetes/">docker
+            documentation</a> on how to enable the Kubernetes server in docker-desktop and then select
+            docker-desktop context.
+            For other type of k8s clusters, check if kube-apiserver of your k8s cluster is reachable from your
+            host machine.
+        </Alert>
+    </Box>
+        }
+    return (
     <>
-      <Typography variant="h3">Calyptia Core extension</Typography>
-      <Typography variant="body1" color="text.secondary" sx={{ mt: 2 }}>
+        <Typography variant="h3">Calyptia Core extension</Typography>
+        <Typography variant="body1" color="text.secondary" sx={{ mt: 2 }}>
         Calyptia Core Docker Desktop extension
-      </Typography>
+        </Typography>
 
-      <Stack direction="row" alignItems="start" spacing={2} sx={{ mt: 4 }}>
-      {component}
-      </Stack>
+        <Stack direction="row" alignItems="start" spacing={2} sx={{ mt: 4 }}>
+        {component}
+        </Stack>
     </>
-  );
-}
+    );
 }
