5b17fa5716335be5fc004e0b32b3891c77e427bc
Nicolás Parada <nicolas@calyptia.com>
2022-10-14 13:49:40 -0300

    react query and core instance refetch

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,15 @@
 import { createTheme, ThemeProvider } from "@mui/material"
 import Container from "@mui/material/Container"
 import CssBaseline from "@mui/material/CssBaseline"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import AuthGuard from "./components/AuthGuard"
 import CoreInstanceScreen from "./components/CoreInstanceScreen"
 import Header from "./components/Header"
 import { AuthClientProvider } from "./hooks/auth"
 import { DockerDesktopClientProvider } from "./hooks/docker-desktop"
 import { ProjectTokenProvider } from "./hooks/project-token"
+
+const queryClient = new QueryClient()
 
 const theme = createTheme({
     palette: {
@@ -25,22 +28,24 @@ export function App() {
         <DockerDesktopClientProvider>
             <ThemeProvider theme={theme}>
                 <CssBaseline />
-                <AuthClientProvider
-                    auth0Domain={process.env.REACT_APP_AUTH0_DOMAIN}
-                    auth0ClientID={process.env.REACT_APP_AUTH0_CLIENT_ID}
-                    auth0Audience={process.env.REACT_APP_AUTH0_AUDIENCE}
-                >
-                    <AuthGuard cloudBaseURL={process.env.REACT_APP_CLOUD_BASE_URL}>
-                        <ProjectTokenProvider>
-                            <Header />
-                            <br />
-                            <Container>
-                                <CoreInstanceScreen />
-                                {/* <Core /> */}
-                            </Container>
-                        </ProjectTokenProvider>
-                    </AuthGuard>
-                </AuthClientProvider>
+                <QueryClientProvider client={queryClient}>
+                    <AuthClientProvider
+                        auth0Domain={process.env.REACT_APP_AUTH0_DOMAIN}
+                        auth0ClientID={process.env.REACT_APP_AUTH0_CLIENT_ID}
+                        auth0Audience={process.env.REACT_APP_AUTH0_AUDIENCE}
+                    >
+                        <AuthGuard cloudBaseURL={process.env.REACT_APP_CLOUD_BASE_URL}>
+                            <ProjectTokenProvider>
+                                <Header />
+                                <br />
+                                <Container>
+                                    <CoreInstanceScreen />
+                                    {/* <Core /> */}
+                                </Container>
+                            </ProjectTokenProvider>
+                        </AuthGuard>
+                    </AuthClientProvider>
+                </QueryClientProvider>
             </ThemeProvider>
         </DockerDesktopClientProvider>
     )
